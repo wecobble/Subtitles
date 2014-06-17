98b55375e9f5ca61f2f354686b7639c578106592
Philip Arthur Moore <philip@pressbuild.com>
2014-06-17 21:45:51 +0700

    Prepare Subtitles for automated testing.

---
+++ tests/README.md
@@ -0,0 +1,1 @@
+# _Subtitles_ Tests
---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Subtitles Unit Tests Bootstrap
+ *
+ * @since 1.0.1
+ */
+
+/**
+ * Turn on error reporting.
+ *
+ * @since 1.0.1
+ */
+error_reporting( E_ALL ); // Report all PHP Errors.
+ini_set( 'error_reporting', E_ALL ); // Same as above.
+
